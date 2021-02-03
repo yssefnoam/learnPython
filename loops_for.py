13de59b2db393fb13a9e16230b0ee5a75a9fa75b
yssefnoam <2020noam@gmail.com>
2021-02-03 17:55:55 +0100

    loops

---
+++ loops_for.py
@@ -0,0 +1,5 @@
+def main():
+    l = [1,2,3,"hi", 3.5]
+    for item in l:
+        print(item)
+main()
---
+++ loops_nested_loop.py
@@ -0,0 +1,11 @@
+def main():
+    print("Programme start")
+    i = 0
+    while (i < 5):
+        j = 0
+        while (j < 5):
+            print("i = {} ; j {}".format(i,j))
+            j = j + 1
+        i = i + 1
+    print("Programme end")
+main()
